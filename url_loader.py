a30e8537c6ce3231ffd2bf9f47fa4803a7791a93
Ryan Rudes <ryanrudes@gmail.com>
2020-07-23 15:11:28 -0400

    Create url_loader.py

---
+++ url_loader.py
@@ -0,0 +1,13 @@
+def encode_url(url):
+  return "".join(url.split("/")[-3::2]).replace(".JPEG", "")
+
+def decode_url(url):
+  folder = url[:url.index("n")]
+  synset = url[url.index("n"):url.index("_")]
+  number = url[url.index("n"):]
+  return "https://storage.googleapis.com/simclr_dataset_bucket/simclr/data/Imagenet/Imagenet%20Divided/" + folder + "/" + synset + "/" + number + ".JPEG"
+
+with open("./image_urls.txt", "w") as f:
+  f.write("\n".join([decode_url(url) for url in open("simclr_code/image_links.txt", "r").read().split("\n")]))
+
+f.close()
