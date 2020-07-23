e04af352915abbaa6666cc6fdecae9395f98f5dc
Ryan Rudes <ryanrudes@gmail.com>
2020-07-23 15:28:53 -0400

    Update url_loader.py

--- url_loader.py
+++ url_loader.py
@@ -7,7 +7,7 @@ def decode_url(url):
   number = url[url.index("n"):]
   return "https://storage.googleapis.com/simclr_dataset_bucket/simclr/data/Imagenet/Imagenet%20Divided/" + folder + "/" + synset + "/" + number + ".JPEG"
 
-with open("simclr_storage_queue/image_urls_encoded.txt", "w") as f:
-  f.write("\n".join([decode_url(url) for url in open("simclr_storage_queue/image_urls_decoded.txt", "r").read().split("\n")]))
+with open("simclr_storage_queue/image_urls_decoded.txt", "w") as f:
+  f.write("\n".join([decode_url(url) for url in open("simclr_storage_queue/image_urls_encoded.txt", "r").read().split("\n")]))
 
 f.close()
